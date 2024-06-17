aa93609cbeda4fe0ec1508b0dd48d9b2950b3353
Alexandre <alexandre.caissy@norda.com>
2024-06-17 18:01:22 -0400

    feat: testing in prod

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,8 +1,22 @@
+// import { unstable_noStore } from "next/cache";
 import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+
+// File based cache config. Needs to be named 'revalidate' || dynamic
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic" //=> like cache:'no-store';
 
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages");
-  const messages = await response.json();
+  // unstable_noStore();
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: {
+  //     // revalidate: 5, // Revalidate every 5s
+  //     tags: ["msg"],
+  //   },
+  // });
+  // const messages = await response.json();
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
